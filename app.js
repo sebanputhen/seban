a480cb3f90d88d33a92c7a8d9887b98767bc8860
tom4k <tom4kurian@gmail.com>
2024-04-05 15:10:31 +0530

    deploy on vercel

--- app.js
+++ app.js
@@ -26,3 +26,5 @@ app.get('/', (req, res) => res.send('Hello world!'));
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+module.exports = app;
