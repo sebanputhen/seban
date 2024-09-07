64d3c691202bf20c8ee1189294f2bea42df41506
Helixjoe <joeljohn5112@gmail.com>
2024-09-07 10:35:51 +0530

    netlify deploy origin added

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:8082",
   "https://tithe-backend.onrender.com",
   "http://localhost:5173",
+  "https://titheajce.netlify.app",
 ];
 
 module.exports = allowedOrigins;
